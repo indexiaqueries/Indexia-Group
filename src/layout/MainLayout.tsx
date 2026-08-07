542e03c4cce18167d8db11d7fc1b3d8b76df4e74
Tanisha Vyas <165962059+tanishavyastv@users.noreply.github.com>
2026-08-07 15:18:28 +0530

    push

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
